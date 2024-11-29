adda2700b97acae235d1e81903a7b0ee7af52c67
Shivom Kumar <73796840+shivomkumar@users.noreply.github.com>
2024-11-29 14:45:33 +0530

    Add files via upload
    Signed-off-by: Shivom Kumar <73796840+shivomkumar@users.noreply.github.com>

---
+++ partials-front/footer.php
@@ -0,0 +1,33 @@
+<section class="social">
+
+   <div class="container text-center">
+
+      <div class="box">
+         
+         <a href="index.php"> <i class="fas fa-angle-right"></i> Home</a>
+         <a href="foods.php"> <i class="fas fa-angle-right"></i> Food</a>
+         <a href="categories.php"> <i class="fas fa-angle-right"></i> Category</a>
+         <a href="index.php"> <i class="fas fa-angle-right"></i> contact</a>
+      </div>
+
+      <div class="box">
+         <h3>CONTACT</h3>
+         <p> <i class="fas fa-phone"></i> SHIVOM </p>
+         <p> <i class="fas fa-phone"></i> +111-222-3333 </p>
+         <p> <i class="fas fa-envelope"></i> shivom1901287@gmail.com </p>
+         <p> <i class="fas fa-map-marker-alt"></i> punjab, india - 143521 </p>
+      </div>
+
+      <div class="box">
+         <h3>FOLLOW US</h3>
+         <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+         <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+         <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+         <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+      </div>
+
+   </section>
+
+   <p class="credit"> &copy; copyright @ <?= date('Y'); ?> by <span>Shivom</span> | all rights reserved! </p>
+
+</footer>
